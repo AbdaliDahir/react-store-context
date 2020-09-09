2a9d68fe3e62ed87ed6767ac85eeeb5eb51b1745
abdaliDahir <abdali.dahir@gmail.com>
2020-09-09 10:23:33 +0100

    stripe config

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51HL5ARCsdcPJkOJWez9s4o5wYBOm2namHYYcQcdbY1iYDKScqsixuM7LW1AaceDfCsfK4UwJ1RGrbMlUog4gy2A100ogxoyyXL';
 
   const onToken = token => {
     console.log(token);
@@ -16,7 +16,7 @@ const StripeCheckoutButton = ({ price }) => {
       name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image='https://avatars3.githubusercontent.com/u/20466978?s=460&u=9283489062ea9a3ca62376dc520bddc592ded07e&v=4'
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
